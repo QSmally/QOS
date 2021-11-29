9e6c0f1301450bccb8f9c8e754cac09b6a5eb957
QSmally <github@qbot.eu>
2021-11-29 20:14:06 +0100

    Type: `kernel` call type

---
+++ boot/type/kernel.s
@@ -0,0 +1,10 @@
+@ENUM kernel
+    @DECLARE fork 0
+    @DECLARE terminate 1
+    @DECLARE swap_point 2
+    @DECLARE allocate_page 3
+    @DECLARE allocate 4
+    @DECLARE drop 5
+    @DECLARE call 6
+    @DECLARE return 7
+@END
