e3b5d998e8dfdccf766902171f484019bf7ebb28
QSmally <github@qbot.eu>
2021-12-23 21:03:08 +0100

    Kernel: abuse argument stack to avoid overriding register

--- kernel/entry.s
+++ kernel/entry.s
@@ -3,15 +3,13 @@
 // The gateway of kernel/OS calls which are made with 'ENT', automatically enters
 // kernel mode and swaps to the right call page.
 
-@DECLARE call_id 1
-
 .main:
     PPL
     RSH 0
-    RST @call_id
+    PPS
     CND #underflow
     JMP .syscall
     @CALL kernel.swap
 .syscall:
-    @MMUDYNAMICARG AST @call_id
+    @MMUDYNAMICARG PPL
     @MMU @mmu.kernel_intermediate_load
