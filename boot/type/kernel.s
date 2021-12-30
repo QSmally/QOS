0e1ac71a0f3ad58b8ac29b1ed6777ea39bc1f042
QSmally <github@qbot.eu>
2021-12-30 21:24:58 +0100

    Kernel: provide bitmap calculations

--- boot/type/kernel.s
+++ boot/type/kernel.s
@@ -1,13 +1,13 @@
 @ENUM kernel
-    @DECLARE fork 0b00000000
-    @DECLARE terminate 0b00000011
-    @DECLARE swap_point 0b00000100
-    @DECLARE allocate_page 0b00000110
-    @DECLARE allocate 0b00001000
-    @DECLARE drop 0b00001010
-    @DECLARE data_store 0b00001100
-    @DECLARE data_load 0b00001110
-    @DECLARE segment_load 0b00010000
-    @DECLARE call 0b00010010
-    @DECLARE return 0b00010101
+    @DECLARE fork 0b00000000            ; 0x00 << 1 | 0
+    @DECLARE terminate 0b00000011       ; 0x01 << 1 | 1
+    @DECLARE swap_point 0b00000100      ; 0x02 << 1 | 0
+    @DECLARE allocate_page 0b00000110   ; 0x03 << 1 | 0
+    @DECLARE allocate 0b00001000        ; 0x04 << 1 | 0
+    @DECLARE drop 0b00001010            ; 0x05 << 1 | 0
+    @DECLARE data_store 0b00001100      ; 0x06 << 1 | 0
+    @DECLARE data_load 0b00001110       ; 0x07 << 1 | 0
+    @DECLARE segment_load 0b00010000    ; 0x08 << 1 | 0
+    @DECLARE call 0b00010010            ; 0x09 << 1 | 0
+    @DECLARE return 0b00010101          ; 0x0A << 1 | 1
 @END
