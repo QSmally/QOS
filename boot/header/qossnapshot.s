266e2e64341403308922d13842ae582f26b51e6f
QSmally <github@qbot.eu>
2022-01-10 19:17:33 +0100

    Header: add `QOSSNAPSHOT` call header

---
+++ boot/header/qossnapshot.s
@@ -0,0 +1,10 @@
+@HEADER QOSSUBROUTINE instruction
+
+// A partially-returnable kernel/OS call that pushes the lower return address onto
+// the call stack.
+
+@DECLARE return_label %random
+
+CPS 0, .@return_label
+@QOS @instruction
+.@return_label:
