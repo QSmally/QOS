5cc9b19b3ac79ccb376673c1012513eeda7615bc
QSmally <github@qbot.eu>
2022-01-22 14:23:14 +0100

    Header: add `SWAPSTACKHEAD` utility header

---
+++ boot/header/swapstackhead.s
@@ -0,0 +1,8 @@
+@HEADER SWAPSTACKHEAD register
+
+// Exchanges the head of the stack with a register, and appends the existing
+// register value to the the parameter stack.
+
+PPL
+XCH @register
+PPS 0
