95132a10b5d0092d557c955ed338e66edf803f9a
QSmally <github@qbot.eu>
2022-02-27 17:39:00 +0100

    QOS: comply with kernel defaults

--- kernel/entry.s
+++ kernel/entry.s
@@ -8,11 +8,25 @@
 // configure the address for the ENT instruction, generate an MMU enumeration
 // automatically and construct the address array for '(kernel_)intermediate_load'.
 
-; main
+.swapnt_map:
+    0 ; 0, fork
+    1 ; 1, terminate
+    0 ; 2, swap point
+    0 ; 3, allocate page
+    0 ; 4, allocate segment
+    0 ; 5, drop
+    0 ; 6, set data target
+    0 ; 7, reset data target
+    0 ; 8, segment load
+    0 ; 9, call
+    1 ; 10, return
+    1 ; 11, interrupt return
+
+.main(16):
     PPL
-    RSH 0
-    PPS 0
-    BRH #underflow, .syscall
+    PPS 0 ; TODO: stack-peek instruction
+    MLD 7, .swapnt_map
+    BRH #!zero, .syscall
     @CALL kernel.swap
 .syscall:
     PPL
