db8ea82f2ad9ea8f3e97ce09576a2312c80256e5
QSmally <github@qbot.eu>
2022-03-28 20:23:54 +0200

    OS: login keypress interrupt task

--- os/login.s
+++ os/login.s
@@ -1,23 +1,25 @@
 @PAGE 5 1
 @ADDRESSABLE os.login
+@OVERFLOWABLE
 
 @DECLARE string_iterator 1
 
 ; main
     IMM @string_iterator, .login_string
-.load_char:
+.print_char:
     MLD @string_iterator, 0
     BRH #zero, .accept_input
 ; print character
     PST zer, @port.io
     INC @string_iterator
-    JMP zer, .load_char
+    JMP zer, .print_char
 .accept_input:
-    // PPI, @interrupt.keypress
-    // @QOSSUBROUTINE @kernel.interrupt
-    @QOSSUBROUTINE @kernel.reschedule
-    JMP zer, .accept_input
+    PPI, @interrupt.keypress
+    @QOSSUBROUTINE @kernel.interrupt
 
 .login_string:
     $login, 0x20, $>
+    0x00
+.passwd_string:
+    $password, 0x20, $>
     0x00
