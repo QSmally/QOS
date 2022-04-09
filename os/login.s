97f7eadfd9b8cbd0281308492e861b61cf971197
QSmally <github@qbot.eu>
2022-04-09 22:27:25 +0200

    QOS: login loop and typing input

--- os/login.s
+++ os/login.s
@@ -2,26 +2,53 @@
 @ADDRESSABLE os.login
 @OVERFLOWABLE
 
-@DECLARE string_iterator 1
+@DECLARE enter_key 1
+@DECLARE string_iterator 2
+@DECLARE key 3
 
 ; main
-    IMM @string_iterator, .login_string
     PST zer, @port.terminal_newline
+    IMM @enter_key, 0x0A
+; login username
+    PPI, 1
+    IMM @string_iterator, .login_string
+    CAL zer, .print_char
+; login password
+    PST zer, @port.terminal_newline
+    PPI, 0
+    IMM @string_iterator, .passwd_string
+    CAL zer, .print_char
+; postlogin task
+    PST zer, @port.terminal_newline
+    @QOS @kernel.terminate
+
 .print_char:
     MLD @string_iterator, 0
-    BRH #zero, .start_input
+    BRH #zero, .accept_input
 ; print character
     PST zer, @port.terminal_push
     INC @string_iterator
     JMP zer, .print_char
-.start_input:
-    @QOSSUBROUTINE @kernel.reschedule
-.accept_input:
+
+.&accept_input:
     PLD zer, @port.terminal
-    BRH #zero, .start_input
+    BRH #zero, .accept_input
+    RST @key
+    SUB @enter_key
+    BRH #zero, .return
+; optionally print character
+    PPK
+    BRH #zero, .accept_input
+    AST @key
     PST zer, @port.terminal_push
     @GOTO accept_input
+.&return:
+    PPL
+    @RETURN
 
-.login_string:
+.&login_string:
     $login, 0x20, $>
+    0x00
+.&passwd_string:
+    $password, 0x20, $>
     0x00
