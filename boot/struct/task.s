a0a67b75f510883d1a17cfbd8b8b84a35c99e338
QSmally <github@qbot.eu>
2022-10-15 23:24:24 +0200

    Type: add structs, update syscalls

---
+++ boot/struct/task.s
@@ -0,0 +1,5 @@
+#STRUCT task
+    #DECLARE proc_id 0         ; 1
+    #DECLARE upper_reference 1 ; 1
+    #DECLARE lower_reference 2 ; 1
+    #DECLARE _empty 3          ; 1
--- boot/type/syscalls.s
+++ boot/type/syscalls.s
@@ -16,11 +16,12 @@
     #DECLARE allocate_256 0b01001
     #DECLARE allocate_32 0b01010
     #DECLARE drop 0b01011
-    #DECLARE memory_map_range 0b01100
-    #DECLARE memory_map_size 0b01101
-    #DECLARE memory_map_single 0b01110
-    #DECLARE memory_unmap 0b01111
+    #DECLARE memory_map 0b01100
+    #DECLARE memory_map_single 0b01101
+    #DECLARE memory_unmap 0b01110
+    #DECLARE memory_remap 0b01111
     ; files
     #DECLARE file_create 0b10000
     #DECLARE file_delete 0b10001
-    #DECLARE file_path_trace 0b10010
+    #DECLARE file_truncate 0b10010
+    #DECLARE file_open 0b10011
