a0ea08fb842b8b6b84c42ac2094b3a541d440357
QSmally <github@qbot.eu>
2022-10-17 22:06:50 +0200

    Type: update syscalls, dmac

--- boot/type/dmac.s
+++ boot/type/dmac.s
@@ -1,5 +1,8 @@
 #ENUM dmac
     #DECLARE execution_context 0b000
+    #DECLARE mapping_config 0b001
+    #DECLARE mapping_physical 0b010
+    #DECLARE mapping_restore 0b011
     ; ...
     #DECLARE k_off 0b101
     #DECLARE stack_push 0b110
--- boot/type/syscalls.s
+++ boot/type/syscalls.s
@@ -10,18 +10,19 @@
     #DECLARE thread_sign_async 0b00101
     #DECLARE thread_sign_create 0b00110
     ; shared libs
-    #DECLARE shared_libr_call 0b00111
-    #DECLARE return 0b01000
+    #DECLARE segm_jump 0b00111
+    #DECLARE libr_call 0b01000
+    #DECLARE return 0b01001
     ; memory
-    #DECLARE allocate_256 0b01001
     #DECLARE allocate_32 0b01010
-    #DECLARE drop 0b01011
-    #DECLARE memory_map 0b01100
-    #DECLARE memory_map_single 0b01101
-    #DECLARE memory_unmap 0b01110
-    #DECLARE memory_remap 0b01111
+    #DECLARE allocate_pipe 0b01011
+    #DECLARE drop 0b01100
+    #DECLARE memory_map 0b01101
+    #DECLARE memory_map_inode 0b01110
+    #DECLARE memory_unmap 0b01111
     ; files
     #DECLARE file_create 0b10000
     #DECLARE file_delete 0b10001
     #DECLARE file_truncate 0b10010
     #DECLARE file_open 0b10011
+    #DECLARE file_device_write 0b10100
